df23f3d87c14d3cfdf12c723c28691fc91c7b2e0
StivensCh <gschavez@ucundinamarca.edu.co>
2026-05-07 10:49:12 -0500

    CAmbios seguridad corregidos

--- src/main/java/com/example/Mediturno/Configuration/ConfiguracionSeguridad.java
+++ src/main/java/com/example/Mediturno/Configuration/ConfiguracionSeguridad.java
@@ -39,7 +39,15 @@ public class ConfiguracionSeguridad {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/pacientes/**").hasAnyRole("PACIENTE", "ADMINISTRADOR", "RECEPCIONISTA")
-                        .requestMatchers("/api/medicos/**").hasAnyRole("MEDICO", "ADMINISTRADOR")
+                        .requestMatchers(
+                                org.springframework.http.HttpMethod.GET,
+                                "/api/medicos/**",
+                                "/api/horarios/**",
+                                "/api/especialidades/**"
+                        ).authenticated()
+
+                        .requestMatchers("/api/medicos/**").hasRole("ADMINISTRADOR")
+                        .requestMatchers("/api/horarios/**").hasRole("ADMINISTRADOR")
                         .requestMatchers("/api/especialidades/**").authenticated()
                         .requestMatchers("/api/turnos/**").authenticated()
                         .requestMatchers("/api/admin/**").hasRole("ADMINISTRADOR")
